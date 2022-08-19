aff986d8025146bd55a6ef6932d9fedda6bda315
unknown <spadadamianezequiel@gmail.com>
2022-08-19 12:03:52 -0300

    se descargo nuevo driver

--- browser_de_youtube.py
+++ browser_de_youtube.py
@@ -1,4 +1,4 @@
-from curses import KEY_ENTER
+#from curses import KEY_ENTER
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -7,7 +7,7 @@ import time
 class buscando_beethoven (unittest.TestCase):
 
     def setUp(self):
-        self.driver = webdriver.Chrome(executable_path=r"C:\Users\Primo\archivos sueltos\Downloads\eeee\TRABAJO\DRIVER\chromedriver.exe")
+        self.driver = webdriver.Chrome(executable_path=r"C:\driver Chrome\chromedriver.exe")
 
     def test_1beethoven(self):
         driver = self.driver
@@ -17,14 +17,18 @@ class buscando_beethoven (unittest.TestCase):
         elemento.send_keys ("youtube")
         elemento.send_keys (Keys.ENTER)
         time.sleep(3)
-        elemento2=driver.find_element_by_class_name("LC20lb MBeuO DKV0Md")
+        
+        
+        elemento2=driver.find_element_by_xpath("//*[@id='rso']/div[1]/div/div/div/div/div/div/div[1]/a/h3")
         elemento2.click()
         time.sleep(3)
-        elemento3=driver.find_element_by_id("search")
+        
+        elemento3=driver.find_element_by_xpath("/html/body/ytd-app/div[1]/div/ytd-masthead/div[3]/div[2]/ytd-searchbox/form/div[1]/div[1]/input")
+        
         elemento3.send_keys("beethoven")
         elemento3.send_keys(Keys.ENTER)
         time.sleep(3)
-        elemento4=driver.find_element_by_class_name("style-scope ytd-video-renderer")
+        elemento4=driver.find_element_by_link_text("Beethoven 🌕 Sonata Claro de Luna (60 Minutos) 🎹 Música Clásica de Piano para Estudiar y Concentrarse")
         elemento4.click()
         time.sleep(30)
 
